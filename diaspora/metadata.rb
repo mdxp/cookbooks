adefa9fa3ced815ba3ebea148117218fc6664820
Marius Ducea <marius.ducea@gmail.com>
2010-09-16 01:43:00 -0700

    diaspora cookbook added

---
+++ diaspora/metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "Promet Solutions"
+maintainer_email "marius@promethost.com"
+license          "Apache 2.0"
+description      "Installs/Configures diaspora"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+%w{ xml imagemagick git build-essential mongodb }.each do |cb|
+  depends cb
+end
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
---
+++ diaspora/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Author:: Marius Ducea (marius@promethost.com)
+# Cookbook Name:: diaspora
+# Recipe:: default
+#
+# Copyright 2010, Promet Solutions
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "build-essential"
+include_recipe "xml"
+include_recipe "imagemagick"
+include_recipe "git"
+include_recipe "mongodb"
+
+gem_package "bundler"
+package "libxslt1-dev"
+
+diaspora_dir = "#{node[:diaspora][:dir]}/diaspora"
+execute "clone diaspora repository" do
+  command "git clone http://github.com/diaspora/diaspora.git"
+  cwd "#{node[:diaspora][:dir]}"
+  not_if "test -d #{diaspora_dir}"
+end
+
+execute "bundle install" do
+  command "bundle install"
+  cwd "#{diaspora_dir}"
+  not_if "cd #{diaspora_dir}; bundle check |grep 'dependencies are satisfied'"
+end
