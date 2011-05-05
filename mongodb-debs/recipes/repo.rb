91dfd1f7d3099c6bae339aef87dd9c81f8513abb
George Miroshnykov <gmiroshnykov@lohika.com>
2011-05-05 11:38:48 +0300

    [mongodb-debs] fixed permissions of /etc/apt/sources.list.d/mongodb.list

--- mongodb-debs/recipes/repo.rb
+++ mongodb-debs/recipes/repo.rb
@@ -8,9 +8,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,9 @@ execute "install mongodb key" do
   not_if "apt-key list | grep 7F0CEB10"
 end
 
-template "/etc/apt/sources.list.d/mongodb.list"
+template "/etc/apt/sources.list.d/mongodb.list" do
+  mode 0644
+end
 
 execute "update apt" do
   command "apt-get update"
